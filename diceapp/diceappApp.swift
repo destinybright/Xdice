2e7c636ce39141b2068ff650458d6725a7bd96b1
destinybright <130610038+destinybright@users.noreply.github.com>
2024-09-26 14:55:04 -0700

    Initial Commit

---
+++ diceapp/diceappApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceappApp.swift
+//  diceapp
+//
+//  Created by Destiny Bright on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct diceappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ diceappTests/diceappTests.swift
@@ -0,0 +1,17 @@
+//
+//  diceappTests.swift
+//  diceappTests
+//
+//  Created by Destiny Bright on 9/26/24.
+//
+
+import Testing
+@testable import diceapp
+
+struct diceappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
